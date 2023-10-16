4e4c7cfe6763f1023afccaae5d90702f95a47af6
Oleksii Samoliuk <samoluk.aleksey@gmail.com>
2023-10-16 16:05:53 +0300

    first commit

---
+++ find_max_projects_client.sql
@@ -0,0 +1,5 @@
+SELECT client.ID, client.NAME, COUNT(project.ID) AS project_count
+FROM client
+LEFT JOIN project ON client.ID = project.CLIENT_ID
+GROUP BY client.ID, client.NAME
+HAVING COUNT(project.ID) = (SELECT MAX(project_count) FROM (SELECT COUNT(ID) AS project_count FROM project GROUP BY CLIENT_ID))
